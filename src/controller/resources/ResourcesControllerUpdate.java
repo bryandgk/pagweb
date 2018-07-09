5fed3cb3e5b34fdaec942f44a4dd77abb68a7095
bryandgk <40638481+bryandgk@users.noreply.github.com>
2018-07-08 22:06:23 -0500

    Update ResourcesControllerUpdate.java

--- src/controller/resources/ResourcesControllerUpdate.java
+++ src/controller/resources/ResourcesControllerUpdate.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.jdo.JDOFatalUserException;
 import javax.jdo.PersistenceManager;
@@ -22,30 +23,81 @@ import org.w3c.dom.NodeList;
 
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.users.UserServiceFactory;
 
 import model.entity.Resources;
-
+import model.entity.Role;
+import model.entity.User;
 import pmf.entity.PMF;
 @SuppressWarnings("serial")
 public class ResourcesControllerUpdate extends HttpServlet {
 	private boolean status = true;
-	
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		try{
-		ServletContext context = getServletContext();
-		String fullPath = context.getRealPath("/WEB-INF/web.xml");
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-		ArrayList<String>resource =reso(fullPath);
-		Key k = KeyFactory.createKey(Resources.class.getSimpleName(), new Long(req.getParameter("userId")).longValue());
-		Resources res = pm.getObjectById(Resources.class, k);
 
-		req.setAttribute("resource", resource);
-		req.setAttribute("res", res);
-		req.getRequestDispatcher("/WEB-INF/Views/Resources/update.jsp").forward(req, resp);
-		}
-		catch (JDOFatalUserException e) {
-			resp.sendRedirect("/resources");
-			
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		Key k = KeyFactory.createKey(Resources.class.getSimpleName(), new Long(req.getParameter("userId")).longValue());
+
+		com.google.appengine.api.users.User uGoogle =UserServiceFactory.getUserService().getCurrentUser();
+		String adminMaestro = "bryan96.sc@gmail.com";
+		String error;
+
+		if (uGoogle==null){
+			error = "necesita iniciar sesion.";
+			req.setAttribute("error", error);
+			req.getRequestDispatcher("/WEB-INF/Views/Errors/error5.jsp").forward(req, resp);
+		} else {
+			if(!uGoogle.getEmail().equals(adminMaestro)){
+				String queryUsers = "select from "+User.class.getName()+ " where email== '"+uGoogle.getEmail()+"' && status==true";
+				List<User> searchUsers = (List<User>) pm.newQuery(queryUsers).execute();
+				
+				if(searchUsers.isEmpty()){
+					req.getRequestDispatcher("/WEB-INF/Views/Errors/error2.jsp").forward(req, resp);
+				} else {
+					String  admin_1="Administrador";
+					Long idRoleAdmin = searchUsers.get(0).getIdRole();
+					String queryAdmin = "select from "+Role.class.getName()+" where roles== '"+admin_1+"' && status==true";
+					List<Role> searchAdmin = (List<Role>) pm.newQuery(queryAdmin).execute();
+					boolean entradaAdmin = searchAdmin.get(0).getId().equals(idRoleAdmin);
+					if(searchAdmin.isEmpty()){
+						error = "No se encuentra administrador";
+						req.setAttribute("error", error);
+						req.getRequestDispatcher("/WEB-INF/Views/Errors/error5.jsp").forward(req, resp);
+					} else {
+						if(entradaAdmin){
+							try{
+								ServletContext context = getServletContext();
+								String fullPath = context.getRealPath("/WEB-INF/web.xml");
+								ArrayList<String>resource =reso(fullPath);
+								Resources res = pm.getObjectById(Resources.class, k);
+								req.setAttribute("resource", resource);
+								req.setAttribute("res", res);
+								req.getRequestDispatcher("/WEB-INF/Views/Resources/update.jsp").forward(req, resp);
+							}
+							catch (JDOFatalUserException e) {
+								resp.sendRedirect("/resources");
+							}
+						}
+						else{
+							error = "no tiene accesos de administrador";
+							req.setAttribute("error", error);
+							req.getRequestDispatcher("/WEB-INF/Views/Errors/error5.jsp").forward(req, resp);
+						}
+					}
+				}
+			} else {
+				try{
+					ServletContext context = getServletContext();
+					String fullPath = context.getRealPath("/WEB-INF/web.xml");
+					ArrayList<String>resource =reso(fullPath);
+					Resources res = pm.getObjectById(Resources.class, k);
+					req.setAttribute("resource", resource);
+					req.setAttribute("res", res);
+					req.getRequestDispatcher("/WEB-INF/Views/Resources/update.jsp").forward(req, resp);
+				}
+				catch (JDOFatalUserException e) {
+					resp.sendRedirect("/resources");
+				}
+			}
 		}
 	}
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
@@ -54,10 +106,10 @@ public class ResourcesControllerUpdate extends HttpServlet {
 		Resources resource = pm.getObjectById(Resources.class, k);
 
 		String res = req.getParameter("resource");
-		
+
 		resource.setName(res);
 		resource.setStatus(status);
-		
+
 		resp.sendRedirect("/resources");
 		pm.close();
 
