fbf7b37dcd31d4f82bc443f0212170ee5b4a1f5d
bryandgk <40638481+bryandgk@users.noreply.github.com>
2018-07-08 21:59:40 -0500

    Update AccessControllerAdd.java

--- src/controller/access/AccessControllerAdd.java
+++ src/controller/access/AccessControllerAdd.java
@@ -1,9 +1,7 @@
 package controller.access;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
+
 import java.util.List;
 
 import javax.jdo.PersistenceManager;
@@ -15,65 +13,86 @@ import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import com.google.appengine.api.users.UserServiceFactory;
 
 import model.entity.Access;
 import model.entity.Resources;
 import model.entity.Role;
+import model.entity.User;
 import pmf.entity.PMF;
 @SuppressWarnings("serial")
 public class AccessControllerAdd extends HttpServlet {
 	private boolean status = false;
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String res = req.getParameter("rol");
-		if(res!=null){
-			String rol = req.getParameter("rol");
-			String resource = req.getParameter("resource");
-			status = true;
-			PersistenceManager pm = PMF.get().getPersistenceManager();
-			Access us = new Access(
-					rol , 
-					resource , 
-					status);
-			
-			try{
-				resp.sendRedirect("/access");
-				pm.makePersistent(us);
-			}finally {
-				pm.close();
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		com.google.appengine.api.users.User uGoogle =UserServiceFactory.getUserService().getCurrentUser();
+		String adminMaestro = "bryan96.sc@gmail.com";
+		String error;
+		
+		if(uGoogle== null){
+			error = "necesita iniciar sesion.";
+			req.setAttribute("error", error);
+			req.getRequestDispatcher("/WEB-INF/Views/Errors/error5.jsp").forward(req, resp);
+		} else {
+			if(!uGoogle.getEmail().equals(adminMaestro)){
+				String queryUsers = "select from "+User.class.getName()+" where email == '"+uGoogle.getEmail()+"' && status ==true";
+				List<User> searchUser = (List<User>) pm.newQuery(queryUsers).execute();
+				
+				if(searchUser.isEmpty()){
+					req.getRequestDispatcher("/WEB-INF/Views/Errors/error2.jsp").forward(req, resp);
+				} else {
+					String  admin_1="Administrador";
+					Long idRoleAdmin = searchUser.get(0).getIdRole();
+					String queryAdmin = "select from "+Role.class.getName()+" where roles== '"+admin_1+"' && status==true";
+					List<Role> searchAdmin = (List<Role>) pm.newQuery(queryAdmin).execute();
+					boolean entradaAdmin = searchAdmin.get(0).getId().equals(idRoleAdmin);
+					if(searchAdmin.isEmpty()){
+						error = "No se encuentra administrador";
+						req.setAttribute("error", error);
+						req.getRequestDispatcher("/WEB-INF/Views/Errors/error5.jsp").forward(req, resp);
+					} else {
+						if(entradaAdmin){
+							String query = "select  from " + Role.class.getName();
+							List<Role> roles = (List<Role>) pm.newQuery(query).execute();
+
+							String query2 = "select  from "+ Resources.class.getName();
+							List<Resources> resource = (List<Resources>) pm.newQuery(query2).execute();
+
+							req.setAttribute("resource", resource);
+							req.setAttribute("roles", roles);
+							req.getRequestDispatcher("/WEB-INF/Views/Access/add.jsp").forward(req, resp);
+						}
+						else{
+							error = "no tiene accesos de administrador";
+							req.setAttribute("error", error);
+							req.getRequestDispatcher("/WEB-INF/Views/Errors/error5.jsp").forward(req, resp);
+						}
+					}
+				}
+			} else {
+				String query = "select  from " + Role.class.getName();
+				List<Role> roles = (List<Role>) pm.newQuery(query).execute();
+
+				String query2 = "select  from "+ Resources.class.getName();
+				List<Resources> resource = (List<Resources>) pm.newQuery(query2).execute();
+
+				req.setAttribute("resource", resource);
+				req.setAttribute("roles", roles);
+				req.getRequestDispatcher("/WEB-INF/Views/Access/add.jsp").forward(req, resp);
 			}
-		}
-		else{
-			PersistenceManager pm = PMF.get().getPersistenceManager();
-			String query = "select  from " + Role.class.getName();
-			List<Role> roles = (List<Role>) pm.newQuery(query).execute();
-	
-			String query2 = "select  from "+ Resources.class.getName();
-			List<Resources> resource = (List<Resources>) pm.newQuery(query2).execute();
-			
-			req.setAttribute("resource", resource);
-			req.setAttribute("roles", roles);
-			req.getRequestDispatcher("/WEB-INF/Views/Access/add.jsp").forward(req, resp);
 		}
 	}
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doGet(req, resp);
-		req.getRequestDispatcher("../WEB-INF/Views/Access/add.jsp").forward(req, resp);
-	
-		/*PersistenceManager pm = PMF.get().getPersistenceManager();
-		String rol = req.getParameter("rol");
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		Long rol = Long.parseLong(req.getParameter("rol"));
 		String src = req.getParameter("resource");	
 		status= true;
 		Access b = new Access(rol,src,status);
 		try {
 			pm.makePersistent(b);
-
+			resp.sendRedirect("/access");
 		} catch (Exception e) {
 			e.printStackTrace();
-		}		*/
-		resp.sendRedirect("/access");
+		} 
 	}
 }
