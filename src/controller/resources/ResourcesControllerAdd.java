795707045707a275a5e1a794aff50c9bf6a308f5
bryandgk <40638481+bryandgk@users.noreply.github.com>
2018-07-08 22:04:19 -0500

    Update ResourcesControllerAdd.java

--- src/controller/resources/ResourcesControllerAdd.java
+++ src/controller/resources/ResourcesControllerAdd.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.jdo.JDOObjectNotFoundException;
 import javax.jdo.PersistenceManager;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -15,33 +17,68 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
+
+import com.google.appengine.api.users.UserServiceFactory;
+
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import model.entity.Resources;
+import model.entity.Role;
+import model.entity.User;
 import pmf.entity.PMF;
 @SuppressWarnings("serial")
 public class ResourcesControllerAdd extends HttpServlet {
 	private boolean status = false;
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		ServletContext context = getServletContext();
-		String fullPath = context.getRealPath("/WEB-INF/web.xml");
-		ArrayList<String>resource =reso(fullPath);
-		req.setAttribute("resource", resource);
-		req.getRequestDispatcher("/WEB-INF/Views/Resources/add.jsp").forward(req, resp);
-		/*String resource = req.getParameter("resource");
-
-			PersistenceManager pm = PMF.get().getPersistenceManager();
-			Resources us = new Resources(
-					resource,  
-					status);
-
-			try{
-				resp.sendRedirect("/resources");
-				pm.makePersistent(us);
-			}finally {
-				pm.close();
-			}*/
-
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		com.google.appengine.api.users.User uGoogle =UserServiceFactory.getUserService().getCurrentUser();
+		String adminMaestro = "bryan96.sc@gmail.com";
+		String error;
+		
+		if(uGoogle==null){
+			error = "necesita iniciar sesion.";
+			req.setAttribute("error", error);
+			req.getRequestDispatcher("/WEB-INF/Views/Errors/error5.jsp").forward(req, resp);
+		} else {
+			if (!uGoogle.getEmail().equals(adminMaestro)){
+				String queryUsers = "select from "+User.class.getName()+" where email == '"+uGoogle.getEmail()+"' && status ==true";
+				List<User> searchUsers = (List<User>) pm.newQuery(queryUsers).execute();
+				
+				if(searchUsers.isEmpty()){
+					req.getRequestDispatcher("/WEB-INF/Views/Errors/error2.jsp").forward(req, resp);
+				} else {
+					String  admin_1="Administrador";
+					Long idRoleAdmin = searchUsers.get(0).getIdRole();
+					String queryAdmin = "select from "+Role.class.getName()+" where roles== '"+admin_1+"' && status==true";
+					List<Role> searchAdmin = (List<Role>) pm.newQuery(queryAdmin).execute();
+					boolean entradaAdmin = searchAdmin.get(0).getId().equals(idRoleAdmin);
+					if(searchAdmin.isEmpty()){
+						error = "No se encuentra administrador";
+						req.setAttribute("error", error);
+						req.getRequestDispatcher("/WEB-INF/Views/Errors/error5.jsp").forward(req, resp);
+					} else {
+						if(entradaAdmin){
+							ServletContext context = getServletContext();
+							String fullPath = context.getRealPath("/WEB-INF/web.xml");
+							ArrayList<String>resource =reso(fullPath);
+							req.setAttribute("resource", resource);
+							req.getRequestDispatcher("/WEB-INF/Views/Resources/add.jsp").forward(req, resp);
+						}
+						else{
+							error = "no tiene permisos de administrador";
+							req.setAttribute("error", error);
+							req.getRequestDispatcher("/WEB-INF/Views/Errors/error5.jsp").forward(req, resp);
+						}
+					}
+				}
+			} else {
+				ServletContext context = getServletContext();
+				String fullPath = context.getRealPath("/WEB-INF/web.xml");
+				ArrayList<String>resource =reso(fullPath);
+				req.setAttribute("resource", resource);
+				req.getRequestDispatcher("/WEB-INF/Views/Resources/add.jsp").forward(req, resp);
+			}
+		}
 	}
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PersistenceManager pm = PMF.get().getPersistenceManager();
